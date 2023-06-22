909647b2a0b5e63e59ed23e73b9d8c441a0d1332
YMariya <ymariya2015@gmail.com>
2023-06-23 01:34:13 +0300

    Ex6: Редирект

---
+++ src/test/java/GetRedirect.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetRedirect {
+
+    @Test
+
+    public void testGetRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+
+    }
+}
