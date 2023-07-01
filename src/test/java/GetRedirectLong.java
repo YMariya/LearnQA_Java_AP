a987de9e39b5b6a350a2dafb141fb61c30eb862b
YMariya <ymariya2015@gmail.com>
2023-07-01 20:50:07 +0300

    Ex7: Долгий редирект. Счетчик

--- src/test/java/GetRedirectLong.java
+++ src/test/java/GetRedirectLong.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class GetRedirectLong {
 
     @Test
-    public void testGetRedirectLong()  {
+    public void testGetRedirectLong() {
 
         Response response = RestAssured
                 .given()
                 .redirects()
                 .follow(true)
-                .redirects().max(100)
+              //  .redirects().max(100)
                 .when()
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
@@ -20,4 +20,27 @@ public class GetRedirectLong {
         int statusCode = response.getStatusCode();
         System.out.println("Status code: " + statusCode);
     }
-}
+
+    @Test
+    public void testGetRedirectLong2() {
+        String locationHeader = "https://playground.learnqa.ru/api/long_redirect";
+        int statusCode = 0;
+        int count = 0;
+        while (statusCode != 200) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(true)
+                    .when()
+                    .get(locationHeader)
+                    .andReturn();
+            count++;
+            System.out.println("Количество редиректов: " + count);
+            statusCode = response.getStatusCode();
+            System.out.println("Status code: " + statusCode);
+            locationHeader = response.getHeader("Location");
+        }
+    }}
+
+
+
